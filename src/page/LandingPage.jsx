46c400ff4f8a6c50a167ffff5a0470db40986df4
ApichaJA <Junyatanakorn@gmail.com>
2023-08-11 15:49:39 +0700

    feat: log wallet

--- src/page/LandingPage.jsx
+++ src/page/LandingPage.jsx
@@ -1,18 +1,22 @@
-import { Web3Button } from "@web3modal/react";
+import { useEffect } from "react";
 import Header from "../components/Header";
-import { Link } from "react-router-dom";
-import { useAccount } from "wagmi";
+import { Link, useSearchParams } from "react-router-dom";
 
 const LandingPage = () => {
-  const { address, ...props } = useAccount();
-  console.log({
-    props,
-  });
+  const [searchParams, setSearchParams] = useSearchParams();
+  useEffect(() => {}, [searchParams]);
+
   return (
     <div className="py-4">
       <Header />
       <div className="px-6 mt-9">
         <div className="text-[18px] font-bold">What’s innovator </div>
+        <div className="mt-[15px] pl-2">
+          <p className="text-[14px] leading-5 font-[300] text-[#808080]">
+            {searchParams.get("authToken")}
+            {searchParams.get("walletAddress")}
+          </p>
+        </div>
         <div className="mt-[15px] pl-2">
           <p className="text-[14px] leading-5 font-[300] text-[#808080]">
             Lorem ipsum dolor sit amet consectetur. Fermentum ut lectus augue
@@ -22,20 +26,13 @@ const LandingPage = () => {
           </p>
         </div>
       </div>
-      {!address && (
-        <div className="flex justify-center mt-[10em]">
-          <Web3Button className="width-full" />
-        </div>
-      )}
-      {address && (
-        <div className="mt-12 px-8 fmd:flex md:w-[300px] justify-center m-auto w-full">
-          <Link to="/quiz">
-            <button className="bg-[#9B74DA] px-4 py-5 w-full m-auto text-white rounded-full text-3sm font-bold">
-              Play now
-            </button>
-          </Link>
-        </div>
-      )}
+      <div className="mt-12 px-8 fmd:flex md:w-[300px] justify-center m-auto w-full">
+        <Link to="/quiz">
+          <button className="bg-[#9B74DA] px-4 py-5 w-full m-auto text-white rounded-full text-3sm font-bold">
+            Play now
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
