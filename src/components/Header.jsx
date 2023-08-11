8b7a719df2a2d779a24fc491fe4e5c7a574e7544
ApichaJA <Junyatanakorn@gmail.com>
2023-08-11 14:22:56 +0700

    fix: favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div>
       <div className="flex pl-6">
         <h1 className="text-3xl font-bold">MUZA Techsauce </h1>
-        <img className="ml-2" width={40} src={Logo} alt="Your SVG" />
+        <img className="ml-2" width={50} src={Logo} alt="Your SVG" />
       </div>
       <div className="w-full md:h-[500px] md:rounded-[20px] flex justify-center mt-2">
         <img className="w-full" src={Present} alt="Your SVG" />
